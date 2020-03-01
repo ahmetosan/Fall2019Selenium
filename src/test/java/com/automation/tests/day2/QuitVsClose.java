9fb2dc5d0ddfc489236cdc1b393e7063d3b5f27a
ahmetosan <ahmetpcf@gmail.com>
2020-03-01 17:43:53 -0500

    get the page title

---
+++ src/test/java/com/automation/tests/day2/QuitVsClose.java
@@ -0,0 +1,18 @@
+package com.automation.tests.day2;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class QuitVsClose {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.youtube.com/watch?v=uKj2f5M-HbA");
+        Thread.sleep(3000);
+
+       // driver.close();
+        driver.quit();
+    }
+}
