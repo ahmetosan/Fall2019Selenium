e03fe0bf59e1dc563b8a54a2e6a9929e6c14cdff
ahmetosan <ahmetpcf@gmail.com>
2020-03-01 15:33:45 -0500

    get the page title

--- src/test/java/com/automation/tests/day2/BasicNavigation.java
+++ src/test/java/com/automation/tests/day2/BasicNavigation.java
@@ -14,12 +14,38 @@ public class BasicNavigation {
         ChromeDriver driver = new ChromeDriver();
 
         driver.get("https://www.google.com/");
+        driver.manage().window().maximize();
         Thread.sleep(3000);
         String title = driver.getTitle();
+        String expectedTitle = "Google";
         System.out.println("title = " + title);
+
+
+        if (expectedTitle.equals(title)){
+            System.out.println("Test passed");
+        }else {
+            System.out.println("Test not passed");
+        }
+
+        driver.navigate().to("https://www.amazon.com");
+
+        if (driver.getTitle().toLowerCase().contains("amazon")){
+            System.out.println("Test passed");
+        }else {
+            System.out.println("Test failed");
+        }
+        driver.navigate().back();
+        verifyEquals(driver.getTitle(),"Google");
 
         driver.close();
 
+    }
 
+    public static void verifyEquals(String arg1, String arg2){
+        if(arg1.equals(arg2)){
+            System.out.println("PASSED");
+        }else {
+            System.out.println("FAILED");
+        }
     }
 }
