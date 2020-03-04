f851e30caa6e9f7f033f1319d9265dc89c2c1b21
ahmetosan <ahmetpcf@gmail.com>
2020-03-03 20:14:16 -0500

    submit button

--- src/test/java/com/automation/tests/day3/FindElementsPractice.java
+++ src/test/java/com/automation/tests/day3/FindElementsPractice.java
@@ -2,13 +2,25 @@ package com.automation.tests.day3;
 
 import com.automation.utilities.DriverFactory;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class FindElementsPractice {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
     WebDriver driver = DriverFactory.createDriver("chrome");
     driver.get("http://practice.cybertekschool.com/sign_up");
+        WebElement fullName = driver.findElement(By.name("full_name"));
+        fullName.sendKeys("mister ahmet");
+
+        WebElement email = driver.findElement(By.name("email"));
+        email.sendKeys("sendenkimsein@gmail.com");
+
+        WebElement signUp = driver.findElement(By.name("wooden_spoon"));
+        signUp.click();
+
+        Thread.sleep(3000);
     driver.quit();
 
     }
