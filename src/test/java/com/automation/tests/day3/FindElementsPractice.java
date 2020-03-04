e2cd1c677f14b54dce2e800a08e932e69c9dae64
ahmetosan <ahmetpcf@gmail.com>
2020-03-03 20:53:41 -0500

    submit button

--- src/test/java/com/automation/tests/day3/FindElementsPractice.java
+++ src/test/java/com/automation/tests/day3/FindElementsPractice.java
@@ -11,17 +11,28 @@ public class FindElementsPractice {
     public static void main(String[] args) throws InterruptedException {
     WebDriver driver = DriverFactory.createDriver("chrome");
     driver.get("http://practice.cybertekschool.com/sign_up");
+        Thread.sleep(3000);
         WebElement fullName = driver.findElement(By.name("full_name"));
         fullName.sendKeys("mister ahmet");
-
+        Thread.sleep(3000);
         WebElement email = driver.findElement(By.name("email"));
         email.sendKeys("sendenkimsein@gmail.com");
-
+        Thread.sleep(3000);
         WebElement signUp = driver.findElement(By.name("wooden_spoon"));
         signUp.click();
-
         Thread.sleep(3000);
-    driver.quit();
+    String expected = "Thank you for signing up. Click the button below to return to the home page.";
+
+    WebElement message = driver.findElement(By.className("subheader"));
+    String actual = message.getText();
+
+    if (expected.equals(actual)){
+        System.out.println("pass");
+    }else {
+        System.out.println("fail");
+    }
+
+        driver.quit();
 
     }
 }
