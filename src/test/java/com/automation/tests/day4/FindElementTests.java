0d1b248053849e1109d6c1a2f30caa4cf18b2648
ahmetosan <ahmetpcf@gmail.com>
2020-03-04 20:40:43 -0500

    March 04

--- src/test/java/com/automation/tests/day4/FindElementTests.java
+++ src/test/java/com/automation/tests/day4/FindElementTests.java
@@ -1,5 +1,32 @@
 package com.automation.tests.day4;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
 public class FindElementTests {
+    public static void main(String[] args) throws InterruptedException {
+
+        WebDriverManager.chromedriver().version("79").setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://practice.cybertekschool.com/");
+        Thread.sleep(3000);
+       List<WebElement> links = driver.findElements(By.tagName("a"));
+
+        for (WebElement link: links ) {
+            System.out.println(link.getText());
+            System.out.println(link.getAttribute("href"));
+            System.out.println();
+        }
+
+        driver.quit();
+
+
+
+    }
 
 }
