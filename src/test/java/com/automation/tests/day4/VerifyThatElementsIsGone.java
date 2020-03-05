5bde88610e73d0b4e6ec90c193b20b657d31ff3b
ahmetosan <ahmetpcf@gmail.com>
2020-03-04 21:46:27 -0500

    March 04

--- src/test/java/com/automation/tests/day4/VerifyThatElementsIsGone.java
+++ src/test/java/com/automation/tests/day4/VerifyThatElementsIsGone.java
@@ -1,4 +1,29 @@
 package com.automation.tests.day4;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class VerifyThatElementsIsGone {
+    public static void main(String[] args) throws InterruptedException {
+
+        WebDriverManager.chromedriver().version("79").setup();
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://practice.cybertekschool.com/multiple_buttons");
+        driver.findElement(By.id("disappearing_button")).click();
+        Thread.sleep(2000);
+
+        if (driver.findElements(By.id("disappearing_button")).size()==0){
+            System.out.println("TEST PASSED");
+        }else {
+            System.out.println("TEST FAILED");
+        }
+
+
+
+
+
+    }
 }
