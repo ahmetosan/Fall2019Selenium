26d97d1046f6330273f5aaccb7a8ec9169a266f3
ahmetosan <ahmetpcf@gmail.com>
2020-03-07 17:12:55 -0500

    March 07 (upload file)

---
+++ src/test/java/com/automation/tests/day5/FileUplodTest.java
@@ -0,0 +1,28 @@
+package com.automation.tests.day5;
+
+import com.automation.utilities.BrowserUtils;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FileUplodTest {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriverManager.chromedriver().version("79").setup();
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("http://practice.cybertekschool.com/upload");
+        BrowserUtils.wait(3);
+
+        WebElement upload = driver.findElement(By.id("file-upload"));
+        String filePath =  System.getProperty("user.dir") + "/pom.eml";
+
+        upload.sendKeys(filePath);
+
+        BrowserUtils.wait(3);
+        driver.quit();
+
+
+    }
+}
