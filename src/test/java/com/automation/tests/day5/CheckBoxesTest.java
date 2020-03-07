e5fe81736875747b50287dd952dde17cf0d20c9d
ahmetosan <ahmetpcf@gmail.com>
2020-03-07 15:04:07 -0500

    March 07

--- src/test/java/com/automation/tests/day5/CheckBoxesTest.java
+++ src/test/java/com/automation/tests/day5/CheckBoxesTest.java
@@ -1,18 +1,43 @@
 package com.automation.tests.day5;
 
+import com.automation.utilities.BrowserUtils;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.List;
+
 public class CheckBoxesTest {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         WebDriverManager.chromedriver().version("79").setup();
         WebDriver driver = new ChromeDriver();
         driver.get("http://practice.cybertekschool.com/checkboxes");
 
+        List<WebElement> checkBoxes = driver.findElements(By.tagName("input"));
+        if(!checkBoxes.get(0).isSelected() && checkBoxes.get(1).isSelected()){
+            System.out.println("TEST PASSED");
+        }else{
+            System.out.println("TEST FAILED");
+        }
+
+        BrowserUtils.wait(4);
+        WebElement checkBox1 = checkBoxes.get(0);
+        checkBox1.click();
+        BrowserUtils.wait(2);
+
+        if (checkBox1.isSelected()){
+            System.out.println("test paased");
+            System.out.println("check box1 selected");
+        }else {
+            System.out.println("test failed" );
+            System.out.println("checked 1 is not selected");
+        }
 
 
+        driver.quit();
 
 
 
