4b6e8cbb4d8398d17ae8e4126b95bd1f90e4d5ae
ahmetosan <ahmetpcf@gmail.com>
2020-03-08 17:10:15 -0400

    March 08 (alert)

--- src/test/java/com/automation/tests/day6/Xpath.java
+++ src/test/java/com/automation/tests/day6/Xpath.java
@@ -9,18 +9,19 @@ import org.openqa.selenium.WebElement;
 public class Xpath {
     public static void main(String[] args) throws InterruptedException {
         WebDriver driver = DriverFactory.createDriver("chrome");
-        driver.get("http://practice.cybertekschool.com/javascript_alerts");
+        driver.get("http://practice.cybertekschool.com/multiple_buttons");
         BrowserUtils.wait(3);
-
+        //value can be inside '' or ""
+        //if you don't know the tag name, or want to skip tag name, use *
+        //for example //*[@onclick='button1()']  | * means any tag name
         WebElement btn1 = driver.findElement(By.xpath("//button[@onclick='button1()']"));
         btn1.click();
-
         WebElement result = driver.findElement(By.id("result"));
         System.out.println(result.getText());
-
-
+        //click on button #2
+        WebElement btn2 = driver.findElement(By.xpath("//button[text()='Button 2']"));
+        btn2.click();
         BrowserUtils.wait(3);
         driver.quit();
-
     }
 }
