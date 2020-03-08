576d9d97c7221877d296f350ef18efc8f52786b8
ahmetosan <ahmetpcf@gmail.com>
2020-03-08 12:45:50 -0400

    March 08 (selectedMultipleOptions)

--- src/test/java/com/automation/tests/day6/SelectedMultipleOptions.java
+++ src/test/java/com/automation/tests/day6/SelectedMultipleOptions.java
@@ -1,5 +1,35 @@
 package com.automation.tests.day6;
 
-public class SelectedMultipleOptions {
+import com.automation.utilities.BrowserUtils;
+import com.automation.utilities.DriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.Select;
 
+public class SelectedMultipleOptions {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver = DriverFactory.createDriver("chrome");
+        driver.get("http://practice.cybertekschool.com/dropdown");
+        BrowserUtils.wait(3);
+
+        Select languagesSelected = new Select(driver.findElement(By.name("Languages")));
+        boolean isMultiple = languagesSelected.isMultiple();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        BrowserUtils.wait(3);
+        driver.quit();
+    }
 }
+
