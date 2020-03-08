4df5f110fac703db1a0e0536596fe47581777372
ahmetosan <ahmetpcf@gmail.com>
2020-03-08 15:33:22 -0400

    March 08 (alert)

--- src/test/java/com/automation/tests/day6/Alerts.java
+++ src/test/java/com/automation/tests/day6/Alerts.java
@@ -12,22 +12,40 @@ public class Alerts {
     public static void main(String[] args) throws InterruptedException {
         WebDriver driver = DriverFactory.createDriver("chrome");
         driver.get("http://practice.cybertekschool.com/javascript_alerts");
-        BrowserUtils.wait(2);
-
+        BrowserUtils.wait(3);
         List<WebElement> buttons = driver.findElements(By.tagName("button"));
-
-        buttons.get(0).click();
-        BrowserUtils.wait(2);
-
-        driver.switchTo().alert().accept();
-
-
-        BrowserUtils.wait(2);
+        buttons.get(0).click();//to click on the first button
+        BrowserUtils.wait(3);
+        //to get the text from popup message
+        String popupText = driver.switchTo().alert().getText();
+        System.out.println(popupText);
+        driver.switchTo().alert().accept();//to click OK
+        String expected = "You successfully clicked an alert";
+        String actual = driver.findElement(By.id("result")).getText();
+        //if will fail, because there is a typo ##BUG##
+        if(expected.equals(actual)){
+            System.out.println("TEST PASSED");
+        }else {
+            System.out.println("TEST FAILED");
+            System.out.println("Expected: "+expected);
+            System.out.println("Actual:   "+actual);
+        }
+        BrowserUtils.wait(3);
+//####################################################################################
+        buttons.get(1).click();//to click on the 2nd button
+        BrowserUtils.wait(3);
+        //to click cancel
+        driver.switchTo().alert().dismiss(); // result must be: You clicked: Cancel
+        String expected2 = "You clicked: Cancel";
+        String actual2 = driver.findElement(By.id("result")).getText();
+        if(expected2.equals(actual2)){
+            System.out.println("TEST PASSED");
+        }else {
+            System.out.println("TEST FAILED");
+            System.out.println("Expected: "+expected2);
+            System.out.println("Actual:   "+actual2);
+        }
+        BrowserUtils.wait(3);
         driver.quit();
-
-
-
-
-
     }
 }
