7f6cc4eedaab0aaf904054502095a7fd27f4f9e7
ahmetosan <ahmetpcf@gmail.com>
2020-03-08 13:37:23 -0400

    March 08 (noSelectedDropdown)

--- src/test/java/com/automation/tests/day6/SelectedMultipleOptions.java
+++ src/test/java/com/automation/tests/day6/SelectedMultipleOptions.java
@@ -4,7 +4,10 @@ import com.automation.utilities.BrowserUtils;
 import com.automation.utilities.DriverFactory;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
+
+import java.util.List;
 
 public class SelectedMultipleOptions {
     public static void main(String[] args) throws InterruptedException {
@@ -14,18 +17,25 @@ public class SelectedMultipleOptions {
 
         Select languagesSelected = new Select(driver.findElement(By.name("Languages")));
         boolean isMultiple = languagesSelected.isMultiple();
+        System.out.println("isMultiple = " + isMultiple);
 
 
 
+        languagesSelected.selectByVisibleText("Java");
+        languagesSelected.selectByVisibleText("JavaScript");
+        languagesSelected.selectByVisibleText("Python");
 
 
+        List<WebElement> selectedLanguage = languagesSelected.getAllSelectedOptions();
+        for (WebElement eachSelectedLanguage:  selectedLanguage) {
+            System.out.println(eachSelectedLanguage.getText());
+        }
+
+        BrowserUtils.wait(2);
 
 
-
-
-
-
-
+        languagesSelected.selectByVisibleText("Java");
+        languagesSelected.deselectAll();
 
 
         BrowserUtils.wait(3);
