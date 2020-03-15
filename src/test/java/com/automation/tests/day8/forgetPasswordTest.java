3d7edfcc4a0624253836c44393a813ccc166abe2
ahmetosan <ahmetpcf@gmail.com>
2020-03-15 12:04:17 -0400

    March 15 (testNG,review)

---
+++ src/test/java/com/automation/tests/day8/forgetPasswordTest.java
@@ -0,0 +1,24 @@
+package com.automation.tests.day8;
+/**
+ * TASK for 5 minutes
+ * Given user is on the practice landing page
+ * When user navigates to "Forgot password" page
+ * Then user enters his email
+ * And clicks "Retrieve password" button
+ * Then user verifies that message "Your e-mail's been sent!" is displayed
+ */
+//
+//import org.testng.annotations.Test;
+//
+//@Test
+//public void  forgetPasswordTest() throws InterruptedException {
+//        driver.findElement(By.linkText("Forgot Password")).click();
+//        Thread.sleep(1000);
+//        driver.findElement(By.name("email")).sendKeys("efewtrell8c@craigslist.org", Keys.ENTER);
+//        Thread.sleep(1000);
+//        String actual=driver.findElement(By.tagName("h4")).getText();
+//        String expected="Your e-mail's been sent!";
+//        Assert.assertEquals(actual,expected,"message is not match");
+//        }
+//
+
