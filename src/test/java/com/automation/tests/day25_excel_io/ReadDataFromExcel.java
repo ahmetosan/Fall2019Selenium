28e57f8a5550ab1a53868be2482c5009639a3e3e
ahmetosan <ahmetpcf@gmail.com>
2020-04-07 22:02:34 -0400

    April 7 (excel conncetion and reading data from excel end of the class )

--- src/test/java/com/automation/tests/day25_excel_io/ReadDataFromExcel.java
+++ src/test/java/com/automation/tests/day25_excel_io/ReadDataFromExcel.java
@@ -1,4 +1,54 @@
 package com.automation.tests.day25_excel_io;
 
+import org.apache.poi.hssf.model.WorkbookRecordList;
+import org.apache.poi.ss.usermodel.*;
+import org.testng.annotations.Test;
+
+import java.io.File;
+import java.io.IOException;
+
 public class ReadDataFromExcel {
+
+    @Test
+    public void readExcelFileTest() throws IOException {
+    File file = new File("VytrackTestUsers.xlsx");
+
+        Workbook workbook = WorkbookFactory.create(file);
+        Sheet workSheet = workbook.getSheet("QA1-short");
+        Row firstRow = workSheet.getRow(0);
+        Cell firstCell = firstRow.getCell(0);
+
+        String value = firstCell.getStringCellValue();
+        String secondCellValue = firstRow.getCell(1).getStringCellValue();
+
+        System.out.println(value);
+        System.out.println(secondCellValue);
+
+        int lastCell = firstRow.getLastCellNum();
+
+        for (int i = 0; i <lastCell ; i++) {
+            System.out.print(firstRow.getCell(i)+"|");
+        }
+
+        int numberOfRows = workSheet.getLastRowNum();
+        int numberOfRows2 = workSheet.getPhysicalNumberOfRows();
+
+        System.out.println("\nlast index number of the row = " + numberOfRows);
+        System.out.println("\nnumberOfRows2 = " + numberOfRows2);
+
+        System.out.println(" =================================================================== ");
+
+
+        for (int row = 0; row < workSheet.getPhysicalNumberOfRows()  ; row++) {
+            for (int cell = 0; cell<workSheet.getRow(row).getLastCellNum(); cell++){
+                String cellValue =  workSheet.getRow(row).getCell(cell).getStringCellValue();
+                System.out.print("cellValue = " + cellValue+"|");
+            }
+            System.out.println();
+        }
+
+
+
+
+    }
 }
--- src/test/java/com/automation/tests/vytrack/login/NewLoginTest.java
+++ src/test/java/com/automation/tests/vytrack/login/NewLoginTest.java
@@ -4,7 +4,9 @@ import com.automation.pages.LoginPages;
 import com.automation.tests.vytrack.AbstractTestBase;
 import com.automation.utilities.BrowserUtils;
 import com.automation.utilities.Driver;
+import com.automation.utilities.ExcelUtil;
 import org.testng.Assert;
+import org.testng.SkipException;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
@@ -51,5 +53,28 @@ public class NewLoginTest extends AbstractTestBase {
         loginPages.login("wrong", "wrong");
         Assert.assertEquals(loginPages.getWarningMessageText(), "Invalid user name or password.");
         BrowserUtils.getScreenshoot("loginPage");
+    }
+    @Test(dataProvider = "credentialsFromExcel")
+    public void loginTestWithExcel(String execute, String username, String password, String firstname, String lastname, String result) throws InterruptedException {
+        test = report.createTest("Login test for username :: " + username);
+        if (execute.equals("y")) {
+            LoginPages loginPage = new LoginPages();
+            loginPage.login(username, password);
+            test.info("Login as " + username);//log some steps
+            test.info(String.format("First name: %s, Last name: %s, Username: %s", firstname, lastname, username));
+            test.pass("Successfully logged in as " + username);
+        } else {
+            test.skip("Test was skipped for user: " + username);
+            //to skip some test in testng
+            throw new SkipException("Test was skipped for user: " + username);
+        }
+    }
+    @DataProvider
+    public Object[][] credentialsFromExcel() {
+        String path = "VytrackTestUsers.xlsx";
+        String spreadSheet = "QA3-short";
+        ExcelUtil excelUtil = new ExcelUtil(path, spreadSheet);
+        //execute  username   password   firstname  lastname   result
+        return excelUtil.getDataArray();
     }
 }
